fc0fcfae5506cf4034e0ed098abf062e5f0b0504
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2022-01-05 14:57:44 +0530

    about us

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const menubtnhamburger = document.querySelector('.menu-btn-hamburger');
 const menubtn = document.querySelector('.menu-btn');
+const navList = document.querySelector('.nav-list');
+
 let menucheck = false;
 menubtnhamburger.addEventListener('click', () => {
 	if (menucheck === false) {
 		menubtn.classList.add('open');
+		navList.classList.add('active');
 		menucheck = true;
 	} else {
 		menubtn.classList.remove('open');
+		navList.classList.remove('active');
 		menucheck = false;
 	}
 });
