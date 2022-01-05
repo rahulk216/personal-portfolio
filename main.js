031f9ac3cb43ec38b86b0c15a0f443b5eadce24c
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2022-01-05 21:59:44 +0530

    design and pic

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const menubtnhamburger = document.querySelector('.menu-btn-hamburger');
+const menubtnhamburger = document.querySelector('.menu-hamburger');
 const menubtn = document.querySelector('.menu-btn');
 const navList = document.querySelector('.nav-list');
+const closeBtn = document.querySelector('.hamburger i');
 
 let menucheck = false;
 menubtnhamburger.addEventListener('click', () => {
@@ -10,6 +11,13 @@ menubtnhamburger.addEventListener('click', () => {
 		menucheck = true;
 	} else {
 		menubtn.classList.remove('open');
+		navList.classList.remove('active');
+		menucheck = false;
+	}
+});
+
+closeBtn.addEventListener('click', () => {
+	if (menucheck === true) {
 		navList.classList.remove('active');
 		menucheck = false;
 	}
